da80de00a4442c71ac3df786d8ec62fbc9421bdb
kylejschulz <kylschulz@gmail.com>
2020-08-17 16:15:10 -0400

    delete feature added and refactoring html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :article_params, only: [:update, :create]
+  
 
   def show
-      @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +15,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -27,13 +28,31 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find (params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
       render 'edit'
     end
   end
+
+  def destroy
+        
+    @article.destroy
+    redirect_to articles_path
+
+  end
+
+  private 
+  
+  def set_article
+    @article = Article.find (params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
+
 
